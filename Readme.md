b3e3e3ae7f7ea3b86427c81bbb2c7aec2efd7dfb
ecstasy244 <ssarose@gmail.com>
2020-04-07 23:40:24 +0545

    added ejs

--- Readme.md
+++ Readme.md
@@ -1,6 +1,9 @@
+A basic to-do list app using nodejs.
+
 #Node Modules installed
 1. express
 2. body-parser
 3. https
 4. request
 5. nodemon
+6. ejs
--- app.js
+++ app.js
@@ -3,21 +3,31 @@
 const express = require("express");
 const https = require("https");
 const bodyParser = require("body-parser");
+//const ejs = require("ejs");
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
+//using ejs
+app.set('view engine', 'ejs');
+
+//home route
 app.get('/',function (req,res) {
-    res.sendFile(__dirname+"/index.html");
-    let date = new Date() ;
-    // if (date.getDay()== 6 || date.getDay()== 0) {
-    //   res.send("weekend");
-    // }else {
-    //     res.send("work day");
-    // }
-    
+   // res.sendFile(__dirname+"/index.html");
+    let today = new Date() ;
+    let currentDay = today.getDay();
+    let day ="";
+    if (currentDay= 6 || currentDay== 0) {
+        day = "Weekend";
+    }else {
+        day = "Weekday";
+    }
+    //view to render
+    res.render('list', {kindOfDay: day});
+
     
 });
 
+//listen to this port
 app.listen(3000,function(){
 console.log("Server started at port 3000");
 });
